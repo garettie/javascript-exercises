1905dfc5036043367a644df8c2202a3146fe42b7
garettie <coldsnp@gmail.com>
2026-03-16 13:44:27 +0800

    finish foundations/06_repeatString and 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let letters = string.split("");
+  letters.reverse();
+  return letters.join("");
 };
+
+reverseString("123! abc! Hello, Odinite.");
 
 // Do not edit below this line
 module.exports = reverseString;
