4eb540c23947d0a741c93cc1164911b8735428e6
garettie <coldsnp@gmail.com>
2026-03-16 15:30:42 +0800

    finish foundations/06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let str = "";
+  for (let i = 0; i < num; i++) {
+    str += word;
+  }
+  return str;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
