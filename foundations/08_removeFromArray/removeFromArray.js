2e74209019549d1e624c435598a4c454654a0199
garettie <coldsnp@gmail.com>
2026-03-16 14:00:34 +0800

    finish foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...rem) {
+  return array.filter((val) => !rem.includes(val));
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
