ff89f3dc5023044508af7675b41a8567493d617f
garettie <coldsnp@gmail.com>
2026-03-16 14:24:33 +0800

    finish foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (a > b) {
+    let bigger = a;
+    a = b;
+    b = bigger;
+  }
+  let inBetween = [];
+  for (let i = a; i <= b; i++) {
+    inBetween.push(i);
+  }
+  let sum = inBetween.reduce((acc, curr) => acc + curr);
+  return sum;
 };
 
 // Do not edit below this line
