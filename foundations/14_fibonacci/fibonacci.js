8f77092c45f7f39729ecc50029cee2e17823f660
garettie <coldsnp@gmail.com>
2026-03-19 17:31:13 +0800

    finish foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num === 0 || num === "0") return 0;
+  if (num < 0) return "OOPS";
+  let curr = 1;
+  let prev = 0;
+  for (let i = 1; i < num; i++) {
+    let next = curr + prev;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
 };
 
 // Do not edit below this line
