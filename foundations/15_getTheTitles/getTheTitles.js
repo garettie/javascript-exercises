ee7f253ec93f772b0673c197aba02f930d296c67
garettie <coldsnp@gmail.com>
2026-03-19 17:45:09 +0800

    finish foundations/15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (book) {
+  return book.map((info) => info.title);
 };
 
 // Do not edit below this line
